4884fc7f1473937b15549d83c27d8e6e8591117b
Jousef <jousef.m@googlemail.com>
2019-10-20 23:04:46 +0200

    Update
    Added code for eighth video

---
+++ Code/EighthVideo.m
@@ -0,0 +1,45 @@
+% MATLAB Video #8 - Performance Improvements
+% October, 10th 2019
+% Jousef Murad - www.engineered-mind.com
+% MATLAB Version - R2018b
+
+clc, clear, close all
+
+%% Section 1 (Pre-Allocation)
+% Source: https://de.mathworks.com/help/matlab/matlab_prog/preallocating-arrays.html
+
+% Bad Example
+
+tic
+x = 0;
+for k = 2:1000000
+   x(k) = x(k-1) + 5;
+end
+t(1) = toc;
+
+% Better Example
+
+tic
+x = zeros(1, 1000000);
+for k = 2:1000000
+   x(k) = x(k-1) + 5;
+end
+t(2) = toc;
+
+figure(1)
+%% Section 2 - Plot Results
+bar(t)
+set(gca,'xtick',1:2,'xticklabel',{'No-Prealloc','Prealloc'})
+title('Comparison of different implementation')
+ylabel('Time used to compute matrix')
+
+%% Section 3 - Tips & Tricks
+figure(2)
+% Use the "gobjects" function to preallocate arrays for graphics objects
+% (example from MATLAB doc)
+
+h = gobjects(4,1);
+
+for k=1:4
+   h(k) = subplot(2,2,k);
+end
